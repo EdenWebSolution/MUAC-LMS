e16d79797b4a3c1a8898054f3b9d84a9d27d3cc2
Akmal <mohamedakmal03@gmail.com>
2019-04-11 22:41:00 +0530

    login Ui partially done

--- MUAC-LMS.Web/MUAC-LMSUI/src/app/modules/security/login/login.component.ts
+++ MUAC-LMS.Web/MUAC-LMSUI/src/app/modules/security/login/login.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { LoginService } from "./../services/login.service";
-import { LoginModel } from "../models/loginModel";
-import { NotificationService } from "./../../shared/services/notification.service";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from './../services/login.service';
+import { LoginModel } from '../models/loginModel';
+import { NotificationService } from './../../shared/services/notification.service';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
@@ -28,30 +28,32 @@ export class LoginComponent implements OnInit {
 
   getLoginForm() {
     return this.fb.group({
-      username: ["", Validators.required],
-      password: ["", Validators.required]
+      username: ['', Validators.required],
+      password: ['', Validators.required]
     });
   }
 
   btnUserLogin() {
     this.isLoginFormSubmitted = true;
-    if (!this.loginForm.valid) return;
+    if (!this.loginForm.valid){
+      return;
+    }
 
     this.loginModel = Object.assign({}, this.loginModel, this.loginForm.value);
     this.loginService.authenticateUser(this.loginModel).subscribe(
       result => {
-        localStorage.setItem("TokenId", result.token);
+        localStorage.setItem('TokenId', result.token);
         localStorage.setItem(
-          "UserAccessLevel",
-          result.isTeacher ? "Teacher" : "Student"
+          'UserAccessLevel',
+          result.isTeacher ? 'Teacher' : 'Student'
         );
-        this.router.navigate(["/muac/dashboard"]);
+        this.router.navigate(['/muac/dashboard']);
       },
       error => {
         this.notificationService.errorMessage(
           error.message !== undefined && error.message !== null
             ? error.message
-            : "Something went wrong, refresh page again"
+            : 'Something went wrong, refresh page again'
         );
       }
     );
